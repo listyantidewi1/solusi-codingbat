25c5d455bed44b19012d6ef822a4fed542db1e98
listyantidewi2 <just.another.dewi@gmail.com>
2021-09-21 13:29:01 +0700

    21/09/2021

---
+++ coding-bat-solution/combo_string.py
@@ -0,0 +1,22 @@
+"""
+Given 2 strings, a and b,
+return a string of the form short+long+short,
+with the shorter string on the outside and the longer string on the inside.
+The strings will not be the same length, but they may be empty (length 0).
+combo_string('Hello', 'hi') → 'hiHellohi'
+combo_string('hi', 'Hello') → 'hiHellohi'
+combo_string('aaa', 'b') → 'baaab'
+
+Diberikan dua buah string a dan b
+Kembalikan sebuah string dalam bentuk pendek+panjang+pendek, dengan string pendek berada di luar string panjang. String a dan b bisa jadi berbeda panjangnya, tapi bisa jadi juga kosong
+
+combo_string('Hello', 'hi') → 'hiHellohi'
+combo_string('hi', 'Hello') → 'hiHellohi'
+combo_string('aaa', 'b') → 'baaab'
+"""
+
+
+def combo_string(a, b):
+    # kembalikan b + a + b jika panjang a lebih dari panjang b
+    # kembalikan a + b + a jika panjang b lebih dari panjang a
+    return b + a + b if len(a) > len(b) else a + b + a
