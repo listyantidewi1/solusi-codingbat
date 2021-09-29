b6ab3e6e9eb96773a4037bda32a779a30f65b89a
listyantidewi2 <just.another.dewi@gmail.com>
2021-09-29 14:40:51 +0700

    29/09/2021

---
+++ coding-bat-solution/make_abba.py
@@ -0,0 +1,18 @@
+"""
+Given two strings, a and b,
+return the result of putting them together in the order abba,
+e.g. "Hi" and "Bye" returns "HiByeByeHi".
+make_abba('Hi', 'Bye') → 'HiByeByeHi'
+make_abba('Yo', 'Alice') → 'YoAliceAliceYo'
+make_abba('x', 'y') → 'xyyx'
+
+Diberikan dua buah string a dan b
+kembalikan hasil penggabungan kedua string tersebut dengan urutan abba
+make_abba('Hi', 'Bye') → 'HiByeByeHi'
+make_abba('Yo', 'Alice') → 'YoAliceAliceYo'
+make_abba('x', 'y') → 'xyyx'
+"""
+
+
+def make_abba(a, b):
+    return a + b * 2 + a
