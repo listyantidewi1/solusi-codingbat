7bf485671c9e5572c4c208318a6bf61533331685
listyantidewi2 <just.another.dewi@gmail.com>
2021-09-29 14:33:47 +0700

    29/09/2021

---
+++ coding-bat-solution/count_hi.py
@@ -0,0 +1,16 @@
+"""
+Return the number of times that the string "hi"
+appears anywhere in the given string.
+count_hi('abc hi ho') → 1
+count_hi('ABChi hi') → 2
+count_hi('hihi') → 2
+
+Kembalikan jumlah string 'hi' yg muncul di dalam string yg diberikan
+count_hi('abc hi ho') → 1
+count_hi('ABChi hi') → 2
+count_hi('hihi') → 2
+"""
+
+
+def count_hi(str):
+    return str.count('hi')
